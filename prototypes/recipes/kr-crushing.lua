7c060bda5360528c869bc389aeede2b91ac535d6
Immy <mythsotech@gmail.com>
2025-12-06 14:37:34 -0800

    First upload

---
+++ prototypes/recipes/kr-crushing.lua
@@ -0,0 +1,11 @@
+local crushing_lib = require("__Krastorio2__/prototypes/libraries/crushing")
+
+crushing_lib.make_recipe(data.raw.item["concrete"], {
+    category = "kr-crushing",
+    hidden = true,
+    hide_from_player_crafting = true,
+    results = {
+        { type = "item", name = "stone-brick", amount_min = 0, amount_max = 2 },
+        { type = "item", name = "kr-sand", amount_min = 3, amount_max = 8 }
+    }
+})
