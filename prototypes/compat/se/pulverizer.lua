7e6d01c4623156c2b5c05ea78dbc78d6803b5c5c
Dragonfoxing <mythsotech@gmail.com>
2025-12-06 22:03:50 -0800

    Removed basic-crushing from pulverisers in order to preserve the intended use of pulvisers as core crushers and hardened ore murderers.

--- prototypes/compat/se/pulverizer.lua
+++ prototypes/compat/se/pulverizer.lua
@@ -1,4 +1,4 @@
 -- Fix K2 crushers so they can use the Crushing Industry recipes.
-local kr_krush = data.raw["assembling-machine"]["se-pulveriser"]
+--local kr_krush = data.raw["assembling-machine"]["se-pulveriser"]
 
-table.insert(kr_krush.crafting_categories, "basic-crushing")
+--table.insert(kr_krush.crafting_categories, "basic-crushing")
