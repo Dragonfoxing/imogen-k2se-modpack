8a0060987c7206bfad66691222602965d5f2154e
Immy May Rayne <dragonfoxing@Immys-MacBook-Air.local>
2025-12-13 23:42:27 -0800

    Updated dependencies, ensured that Quality is actually optional.

--- prototypes/updates/quality.lua
+++ prototypes/updates/quality.lua
@@ -1,5 +1,9 @@
 local frecipe = require("__fdsl__.lib.recipe")
 
+if not mods["quality"] then
+  return
+end
+
 -- Restore the Krastorio version of the qual1 recipe.
 data.raw.recipe["quality-module"].ingredients = {
   { type = "item", name = "electronic-circuit", amount = 5 },
